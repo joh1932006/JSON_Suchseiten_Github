a94a0a964dd9c7720098ec4b66166f534f88876c
Johannes Hinterholzer <j.hinterholzer@htlkrems.at>
2024-10-29 16:36:44 +0100

    Oberfläche Datenbank auswählen

--- JSON_Suchseiten_Projekt/frontend/database-query-frontend/src/app/app.component.ts
+++ JSON_Suchseiten_Projekt/frontend/database-query-frontend/src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import { APIRequestsComponent } from './apirequests/apirequests.component'; // Pfad zur apirequests-Komponente
+import { APIRequestsComponent } from './apirequests/apirequests.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [APIRequestsComponent, HttpClientModule], // Komponente importieren
+  imports: [APIRequestsComponent, HttpClientModule, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'mein-projekt';
+  databases: string[] = ['Datenbank1', 'Datenbank2', 'Datenbank3'];
+  selectedDatabase: string = '';
 }
